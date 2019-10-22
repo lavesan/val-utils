a20d5315f6bfa2d2f6f00f64335dbce49ce3205b
valdery <valdery.jur@gmail.com>
2019-10-22 09:53:38 -0300

    :sparkles: Adds parse to the keys of the child objects
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 /**
  * @description Receives an object with camel case key names
- * @param {{ ...args }} object 
+ * @param { [key: string]: any } object 
  * @returns Object with snake case key names
  */
 const camelToSnakeCase = function(object) {
-    const entries = Object.entries(object);
-    let obj = {};
-    entries.forEach(([key, value]) => {
-        const snakeKey = key.replace(/[A-Z]/g, function(match) {
-            return `_${match.toLowerCase()}`
+    if (typeof object === 'object') {
+        const entries = Object.entries(object);
+        let obj = {};
+        entries.forEach(([key, value]) => {
+            const snakeKey = key.replace(/[A-Z]/g, function(match) {
+                return `_${match.toLowerCase()}`
+            });
+            // If the value is an object, then it's also parsed to snake case
+            if (typeof value === 'object')
+                obj[snakeKey] = camelToSnakeCase(value);
+            else
+                obj[snakeKey] = value;
         });
-        obj[snakeKey] = value;
-    });
-    return obj;
+        return obj;
+    } else
+        return object;
 }
 
 /**
  * @description Receives a object with snake case key names
- * @param {{ ...args }} object
+ * @param { [key: string]: any } object
  * @returns Object with camel case key names
  */
 const snakeToCamelCase = function(object) {
-    const entries = Object.entries(object);
-    let obj = {};
-    entries.forEach(([key, value]) => {
-        const camelKey = key.replace(/_./g, function(match) { 
-            return match.replace(/_/g, '').toUpperCase(); 
+    if (typeof object === 'object') {
+        const entries = Object.entries(object);
+        let obj = {};
+        entries.forEach(([key, value]) => {
+            const camelKey = key.replace(/_./g, function(match) { 
+                return match.replace(/_/g, '').toUpperCase(); 
+            });
+            // If the value is an object, then it's also parsed to camel case
+            if (typeof value === 'object')
+                obj[camelKey] = snakeToCamelCase(value);
+            else
+                obj[camelKey] = value;
         });
-        obj[camelKey] = value;
-    });
-    return obj;
+        return obj;
+    } else
+        return object;
 }
 
 module.exports = { snakeToCamelCase, camelToSnakeCase }
